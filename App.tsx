1e7ca95312cf430eecd3cdfc696ce441b588c48d
Marcinho <marcio.mrcr@outlook.com>
2023-01-24 15:07:25 -0300

    config nativewind tailwindcss

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@ import {
   Inter_800ExtraBold,
   useFonts,
 } from '@expo-google-fonts/inter';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import Loading from './src/components/Loading';
+import { Home } from './src/screens/Home';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -20,26 +21,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Oi Jesus!</Text>
+    <>
+      <Home />
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-});
