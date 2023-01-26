11d7c3c70acf05304a1496e8195919f23101f624
Marcinho <marcio.mrcr@outlook.com>
2023-01-26 11:53:10 -0300

    add days in home screen

--- App.tsx
+++ App.tsx
@@ -7,7 +7,9 @@ import {
 } from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
 import Loading from './src/components/Loading';
+import './src/lib/dayjs';
 import { Home } from './src/screens/Home';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
